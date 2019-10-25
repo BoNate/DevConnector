95a4416510a54c97739137c049783c31378f5bf0
BoNate <bn@pleronix.com>
2019-10-25 13:21:31 +0300

    routine update

---
+++ models/Profile.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  company: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-//  @route   GET api/users
-//  @desc    Test route
-//  @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//  @route   GET api/profile/me
+//  @desc    Get current users profile
+//  @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//  @route   POST api/profile
+//  @desc    Create current users profile
+//  @access  Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("title", "Title is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { company, title, date } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (title) profileFields.title = title;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+      res;
+    } catch (err) {
+      console.error();
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
